2b437ffecec254e55717c16c482575081368d272
jwitherell24 <witherellj24@gmail.com>
2022-10-12 10:43:21 -0400

    Created route to website

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello from Pet Adoption"
+
+
+@app.route("/meow")
+def cat():
+    return "Meow"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000, host="127.0.0.1")

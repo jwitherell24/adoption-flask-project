b6ff5b4dce8ad12791b26b94563c77857fdd9d3e
jwitherell24 <witherellj24@gmail.com>
2022-10-16 21:34:47 -0400

    Added templates file and layout file and connected each file to layout

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import (Flask, render_template, 
+                   url_for, request)
 
 
 app = Flask(__name__)
@@ -8,6 +9,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/add-pet", methods=["GET", "POST"])
+def add_pet():
+    print(request.form)
+    print(request.form["name"])
+    return render_template("addpet.html")
+
+
+@app.route("/pet")
+def pet():
+    return render_template("pet.html")
 
 
 if __name__ == "__main__":

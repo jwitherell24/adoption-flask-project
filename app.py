0d343730f2367e738918639c0c6077551bf892bf
jwitherell24 <witherellj24@gmail.com>
2022-10-18 16:49:46 -0400

    Created functionality to add pets to database

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import (render_template, 
+from flask import (render_template, redirect,
                    url_for, current_app, request)
 from models import db, Pet, app
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    pets = Pet.query.all()
+    return render_template("index.html", pets=pets)
 
 
 @app.route("/add-pet", methods=["GET", "POST"])
 def add_pet():
-    print(request.form)
-    print(request.form["name"])
+    if request.form:
+        new_pet = Pet(name=request.form["name"], age=request.form["age"],
+                      breed=request.form["breed"], color=request.form["color"],
+                      size=request.form["size"], weight=request.form["weight"],
+                      url=request.form["url"], url_tag=request.form["alt"],
+                      pet_type=request.form["pet"], gender=request.form["gender"],
+                      spay=request.form["spay"], house_trained=request.form["housetrained"],
+                      description=request.form["description"])
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect(url_for("index"))
     return render_template("addpet.html")
 
 

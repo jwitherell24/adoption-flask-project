d3f091ead105d36df7f354f6b9de5ff37b737378
jwitherell24 <witherellj24@gmail.com>
2022-10-19 10:17:53 -0400

    Added edit and delete capabilities for pets in database

--- app.py
+++ app.py
@@ -25,10 +25,40 @@ def add_pet():
     return render_template("addpet.html")
 
 
-@app.route("/pet")
-def pet():
-    return render_template("pet.html")
+@app.route("/pet/<id>")
+def pet(id):
+    pet = Pet.query.get(id)
+    return render_template("pet.html", pet=pet)
 
+
+@app.route("/edit/<id>", methods=["GET", "POST"])
+def edit_pet(id):
+    pet = Pet.query.get(id)
+    if request.form:
+        pet.name = request.form["name"]
+        pet.age = request.form["age"]
+        pet.breed = request.form["breed"]
+        pet.color = request.form["color"]
+        pet.size = request.form["size"]
+        pet.weight = request.form["weight"]
+        pet.url = request.form["url"]
+        pet.url_tag = request.form["alt"]
+        pet.pet_type = request.form["pet"]
+        pet.gender = request.form["gender"]
+        pet.spay = request.form["spay"]
+        pet.house_trained = request.form["housetrained"]
+        pet.description = request.form["description"]
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("editpet.html", pet=pet)
+
+
+@app.route("/delete/<id>")
+def delete_pet(id):
+    pet = Pet.query.get(id)
+    db.session.delete(pet)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     with app.app_context():
